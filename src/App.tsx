1574af0cfda622aeff3af1f0a5268a2805edf557
dwirzkid <dwirizki164@gmail.com>
2024-12-17 22:35:36 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import DashboardPage from './pages/dashboard'
+
+function App() {
+  
+
+  return (
+    <>
+    <DashboardPage />
+    
+    </>
+  )
+}
+
+export default App
